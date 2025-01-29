427b363dc8e1ad16cceb21b3c4a9d5320318b4b9
MagicalInnkeep <TheMagicalInnkeeper@proton.me>
2025-01-29 10:58:34 +0100

    Add functionality to add new books
    Idea is to have a card for each book, with the
    last card being an empty one with a big + inside.
    You click on the empty card and you then can add
    a new book in it.
    Current code should do it, without nice css.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //--------------------------
 
 //Array to keep track of stored goods
-const myLibrary=[];
+const myLibrary = [];
 const library = document.querySelector(".library");
 
 //--------------------------
@@ -19,14 +19,14 @@ function Book(title, author, pages, read) {
 
 Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
-};    
+};
 
 //--------------------------
 // Library Functions
 //--------------------------
 
-function addBookToLibrary(title,author,pages,read){
-    const newBook=new Book(title,author,pages,read);
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
@@ -35,8 +35,86 @@ function addBookToLibrary(title,author,pages,read){
 // DOM Functions
 //--------------------------
 
-function displayBooks(){
-   
+function AddLibrary() {
+
+    const addBookDiv = document.createElement("div");
+    addBookDiv.setAttribute("class", "libraryItem addButton");
+
+    const addBookBtn = document.createElement("button");
+    addBookBtn.textContent = "+";
+    addBookBtn.setAttribute("class", "addButton");
+    addBookBtn.addEventListener('click', (event) => {
+        //Remove the button itself
+        addBookDiv.removeChild(addBookBtn);
+
+        //Add form for input
+        const addBookForm = document.createElement("form");
+
+        //Add labels and inputs:
+        const titleLabel    = document.createElement("label");
+        const authorLabel   = document.createElement("label");
+        const pagesLabel    = document.createElement("label");
+        const readLabel     = document.createElement("label");
+        const titleInput    = document.createElement("input");
+        const authorInput   = document.createElement("input");
+        const pagesInput    = document.createElement("input");
+        const readInput     = document.createElement("input");
+        const btnOk         = document.createElement("button");
+
+        titleLabel.setAttribute("for","titleInput");
+        authorLabel.setAttribute("for","authorInput");
+        pagesLabel.setAttribute("for","pagesInput");
+        readLabel.setAttribute("for","readInput");
+
+        titleLabel.textContent="Title:";
+        authorLabel.textContent="Author:";
+        pagesLabel.textContent="Pages:";
+        readLabel.textContent="Read:";
+
+        titleInput.setAttribute("id","titleInput");
+        authorInput.setAttribute("id","authorInput");
+        pagesInput.setAttribute("id","pagesInput");
+        readInput.setAttribute("id","readInput");
+        titleInput.setAttribute("name","title");
+        authorInput.setAttribute("name","author");
+        pagesInput.setAttribute("name","pages");
+        readInput.setAttribute("name","read");
+        titleInput.setAttribute("type","text");
+        authorInput.setAttribute("type","text");
+        pagesInput.setAttribute("type","integer");
+        readInput.setAttribute("type","checkbox");
+
+        btnOk.textContent="Ok";
+        btnOk.setAttribute("type","button");
+        btnOk.addEventListener('click', (event)=>{
+            const title = titleInput.value;
+            const author = authorInput.value;
+            const pages = parseInt(pagesInput.value, 10); 
+            const read = readInput.checked; 
+
+            addBookToLibrary(title,author,pages,read);
+            displayBooks();
+        });
+
+        addBookForm.appendChild(titleLabel);
+        addBookForm.appendChild(titleInput);
+        addBookForm.appendChild(authorLabel);
+        addBookForm.appendChild(authorInput);
+        addBookForm.appendChild(pagesLabel);
+        addBookForm.appendChild(pagesInput);
+        addBookForm.appendChild(readLabel);
+        addBookForm.appendChild(readInput);
+        addBookForm.appendChild(btnOk);
+
+        addBookDiv.appendChild(addBookForm);
+        titleInput.focus();
+    });
+    addBookDiv.appendChild(addBookBtn);
+    library.appendChild(addBookDiv);
+
+}
+
+function displayBooks() {
     //Clears item before loading new status
     library.innerHTML = "";
 
@@ -45,10 +123,12 @@ function displayBooks(){
         console.log(element.info());
         //Creates template Element
         const libraryItem = document.createElement("div");
-        libraryItem.setAttribute("class","libraryItem");
-        libraryItem.textContent=element.info();
+        libraryItem.setAttribute("class", "libraryItem");
+        libraryItem.textContent = element.info();
         library.appendChild(libraryItem);
     });
+
+    AddLibrary();
 }
 
 //--------------------------
